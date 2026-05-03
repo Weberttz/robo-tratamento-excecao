ef28b9b40bbb53895146b5d8645002258cc0d769
jsfael <faeulimao@gmail.com>
2026-05-03 13:25:36 -0300

    feat: CoresDuplicadas como RuntimeException e update no input de tabuleiro.fxml

--- src/main/java/com/projetorobo/exception/CoresDuplicadasException.java
+++ src/main/java/com/projetorobo/exception/CoresDuplicadasException.java
@@ -1,6 +1,6 @@
 package com.projetorobo.exception;
 
-public class CoresDuplicadasException extends Exception {
+public class CoresDuplicadasException extends RuntimeException {
     public CoresDuplicadasException() {
         super("Os robôs não podem ter a mesma cor!");
     }
