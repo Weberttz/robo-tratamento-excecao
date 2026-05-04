2031da31bc9f42cad7ae61bc35c15e12f31b538c
jsfael <faeulimao@gmail.com>
2026-05-04 13:13:11 -0300

    feat: criacao da telaResultadoFinal

---
+++ src/main/java/com/projetorobo/controllers/TelaResultadoController.java
@@ -0,0 +1,45 @@
+package com.projetorobo.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class TelaResultadoController {
+
+    @FXML
+    private Button btnSair;
+
+    @FXML
+    private Label labelInvalidosRobo1;
+
+    @FXML
+    private Label labelInvalidosRobo2;
+
+    @FXML
+    private Label labelNomeRobo1;
+
+    @FXML
+    private Label labelNomeRobo2;
+
+    @FXML
+    private Label labelStatus;
+
+    @FXML
+    private Label labelTitulo;
+
+    @FXML
+    private Label labelValidosRobo1;
+
+    @FXML
+    private Label labelValidosRobo2;
+
+    @FXML
+    private Label labelVencedor;
+
+    @FXML
+    void fecharJogo(ActionEvent event) {
+
+    }
+
+}
